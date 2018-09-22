ad35f01fadba0514b622502847940d9dfc67f95e
bw1 <bw1@aol.at>
2018-09-22 15:35:37 +0200

    [ddg_async] add head

--- scripts/ddg_async.pl
+++ scripts/ddg_async.pl
@@ -1,9 +1,35 @@
-use Irssi;
+# duckduckgo.pl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
+use Irssi;
 use Mojo::Util 'url_unescape';
 use Mojo::UserAgent;
 use feature 'say';
+use Time::HiRes;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.01';
+%IRSSI = (
+    authors	=> 'vague666',
+    contact	=> 'vague666@users.noreply.github.com',
+    name	=> 'ddg_async',
+    description	=> 'search by duckduckgo',
+    license	=> 'LGPLv3',
+    url		=> 'http://scripts.irssi.org',
+    changed	=> '2018-09-22',
+);
 
 Irssi::command_bind('ddg', 'cmd_wrapper');
 Irssi::signal_add('message public', 'sig_wrapper');
@@ -20,9 +46,11 @@ sub cmd_wrapper {
 
 sub ddg {
   my ($data, $tag, $target) = @_;
+  my $start=Time::HiRes::time();
   my $index = 1;
   my $link="http://duckduckgo.com/lite?q=";
   my $ua=Mojo::UserAgent->new;
+  print "ddg: ",$link . quotemeta $data;
   $ua->max_redirects(10)->max_connections(0)->get_p($link . quotemeta $data)->then(sub {
     my $tx = shift;
     my $server = Irssi::server_find_tag($tag);
@@ -48,4 +76,7 @@ sub ddg {
     return unless $win;
     $win->print("Connection error: $err");
   })->wait;
+  print "ddg: ",Time::HiRes::time()-$start,"s ";
 }
+
+# vim: set sw=2 ts=2 expandtab :
