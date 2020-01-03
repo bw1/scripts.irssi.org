9b32b86e815dd7c0407c7976d7448841c19a3b21
bw1 <bw1@aol.at>
2020-01-03 17:50:01 +0100

    zw

--- scripts/apropos.pl
+++ scripts/apropos.pl
@@ -159,8 +159,9 @@ sub cmd_help {
 sub getfile {
 	my ( $fn ) = @_;
 	my $ff = File::Fetch->new( uri=>$fn );
+	local $File::Fetch::DEBUG=1;
 	my $w = $ff->fetch( to=>$path )
-		or printerror("getfile ($fn) ".$ff->error);
+		or printerror("getfile ($fn) ".$ff->error(1));
 }
 
 sub writetag {
