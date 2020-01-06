51ea1dbd0276303a85fa568c720f8fbc8db1155a
bw1 <bw1@aol.at>
2020-01-06 15:43:31 +0100

    zw

--- scripts/apropos.pl
+++ scripts/apropos.pl
@@ -228,7 +228,7 @@ sub maketags {
 		Irssi::print("file exists", MSGLEVEL_CLIENTCRAP);
 		my $s;
 		my $t;
-		open($fi, '| :utf8', "cat $fn")
+		open($fi, '-|', "cat $fn")
 			or printerror("cannot open < $fn: $!");
 		while ( my $r = <$fi> ) {
 			if ( $r =~ m/^#+(.*?)$/ ) {
